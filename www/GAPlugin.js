51153d014d5297774b2b94085bcda1b7ecd88d55
Clément Vollet <clement@joshfire.com>
2013-09-24 15:51:15 +0200

    Use module syntax for js file

--- www/GAPlugin.js
+++ www/GAPlugin.js
@@ -1,58 +1,58 @@
-(function(){
-    var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
+var GAPlugin = {};
 
-    function GAPlugin() { }
+var exec = require('cordova/exec');
 
-    // initialize google analytics with an account ID and the min number of seconds between posting
-    //
-    // id = the GA account ID of the form 'UA-00000000-0'
-    // period = the minimum interval for transmitting tracking events if any exist in the queue
-    GAPlugin.prototype.init = function(success, fail, id, period) {
-        return cordovaRef.exec(success, fail, 'GAPlugin', 'initGA', [id, period]);
-    };
+/**
+ * Initialize google analytics with an account ID and the min number of seconds
+ * between posting
+ *
+ * @param {string} id - The GA account ID of the form 'UA-00000000-0'
+ * @param {number} period - The minimum interval for transmitting tracking
+ *                          events if any exist in the queue
+ */
+GAPlugin.prototype.init = function(success, fail, id, period) {
+  return exec(success, fail, 'GAPlugin', 'initGA', [id, period]);
+};
 
-    // log an event
-    //
-    // category = The event category. This parameter is required to be non-empty.
-    // eventAction = The event action. This parameter is required to be non-empty.
-    // eventLabel = The event label. This parameter may be a blank string to indicate no label.
-    // eventValue = The event value. This parameter may be -1 to indicate no value.
-    GAPlugin.prototype.trackEvent = function(success, fail, category, eventAction, eventLabel, eventValue) {
-        return cordovaRef.exec(success, fail, 'GAPlugin', 'trackEvent', [category, eventAction, eventLabel, eventValue]);
-    };
+/**
+ * Log an event
+ *
+ * @param {string} category - The event category.
+ * @param {string} eventAction - The event action.
+ * @param {string} [eventLabel] - The event label.
+ * @param {number} eventValue - The event value. This parameter may be -1 to
+ *                              indicate no value.
+ */
+GAPlugin.prototype.trackEvent = function(success, fail, category, eventAction, eventLabel, eventValue) {
+  return exec(success, fail, 'GAPlugin', 'trackEvent', [category, eventAction, eventLabel, eventValue]);
+};
 
 
-    // log a page view
-    //
-    // pageURL = the URL of the page view
-    GAPlugin.prototype.trackPage = function(success, fail, pageURL) {
-        return cordovaRef.exec(success, fail, 'GAPlugin', 'trackPage', [pageURL]);
-    };
+/**
+ * Log a page view
+ *
+ * @param {string} pageURL - The URL of the page view
+ */
+GAPlugin.prototype.trackPage = function(success, fail, pageURL) {
+  return exec(success, fail, 'GAPlugin', 'trackPage', [pageURL]);
+};
 
-    // Set a custom variable. The variable set is included with
-    // the next event only. If there is an existing custom variable at the specified
-    // index, it will be overwritten by this one.
-    //
-    // value = the value of the variable you are logging
-    // index = the numerical index of the dimension to which this variable will be assigned (1 - 20)
-    //  Standard accounts support up to 20 custom dimensions.
-    GAPlugin.prototype.setVariable = function(success, fail, index, value) {
-        return cordovaRef.exec(success, fail, 'GAPlugin', 'setVariable', [index, value]);
-    };
-    
-    GAPlugin.prototype.exit = function(success, fail) {
-        return cordovaRef.exec(success, fail, 'GAPlugin', 'exitGA', []);
-    };
- 
-    if (cordovaRef)
-    {
-        cordovaRef.addConstructor(function() {
-            if(!window.plugins) {
-                window.plugins = {};
-            }
-            if(!window.plugins.gaPlugin) {
-                window.plugins.gaPlugin = new GAPlugin();
-            }
-        });
-    }
-})(); /* End of Temporary Scope. */
+/**
+ * Set a custom variable. The variable set is included with the next event only.
+ * If there is an existing custom variable at the specified index, it will be
+ * overwritten by this one.
+ *
+ * @param {string} value - The value of the variable you are logging
+ * @param {number} index - The numerical index of the dimension to which this
+ *                         variable will be assigned (1 - 20)
+ *                         Standard accounts support up to 20 custom dimensions.
+ */
+GAPlugin.prototype.setVariable = function(success, fail, index, value) {
+  return exec(success, fail, 'GAPlugin', 'setVariable', [index, value]);
+};
+
+GAPlugin.prototype.exit = function(success, fail) {
+  return exec(success, fail, 'GAPlugin', 'exitGA', []);
+};
+
+module.exports = GAPlugin;
